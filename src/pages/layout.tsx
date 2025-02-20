8ee085b8063378f22c7d668b017cc63abd187705
Mirza Zahedi <mohammadkashani220@gmail.com>
2025-02-20 20:14:22 +0330

    added github link to header

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,15 +1,21 @@
 import { Outlet } from "react-router";
 import { Link } from "react-router";
 import { House as HouseIcon } from "lucide-react";
+import GithubIcon from "@/assets/github.svg";
 import Container from "@/components/container";
 
 const MainLayout = () => {
   return (
     <>
       <header className="sticky top-0 z-10 bg-white shadow-md">
-        <Container className="flex gap-4 py-2 justify-center">
+        <Container className="flex justify-center gap-4 py-2">
           <Link to={"/"}>
             <HouseIcon />
+          </Link>
+          <Link
+            to={"https://github.com/mohammadhoseinzahedi/react-mini-projects"}
+          >
+            <img src={GithubIcon} alt="Github Icon" height={24} width={24} />
           </Link>
         </Container>
       </header>
